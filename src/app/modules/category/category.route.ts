815a555178f5fc46a10efbb620562e706f88ed4d
sampod76 <108483839+sampod76@users.noreply.github.com>
2023-09-27 01:42:32 +0600

    add category route

---
+++ src/app/modules/category/category.route.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import { getALLCategory } from './category.controller';
+
+
+const router = express.Router();
+
+router.get('/', getALLCategory);
+
+export const CategoryRouter = router;
